db4ccff2f2c357bebc0cbd7fe5076e2d371d174d
richardlford <richardlford@gmail.com>
2021-11-23 18:16:23 -0500

    Add FunctionFixpointBase

---
+++ analyzer/include/ikos/analyzer/analysis/function_fixpoint_base.hpp
@@ -0,0 +1,57 @@
+//
+// Created by fordrl on 11/23/21.
+//
+
+#ifndef IKOS_FUNCTION_FIXPOINT_BASE_HPP
+#define IKOS_FUNCTION_FIXPOINT_BASE_HPP
+
+#include <ikos/ar/semantic/code.hpp>
+#include <ikos/ar/semantic/function.hpp>
+
+#include <ikos/core/fixpoint/fwd_fixpoint_iterator.hpp>
+
+#include <ikos/analyzer/analysis/call_context.hpp>
+#include <ikos/analyzer/analysis/context.hpp>
+#include <ikos/analyzer/analysis/execution_engine/fixpoint_cache.hpp>
+#include <ikos/analyzer/analysis/fixpoint_parameters.hpp>
+#include <ikos/analyzer/analysis/value/abstract_domain.hpp>
+
+namespace ikos {
+namespace analyzer {
+
+class FunctionFixpointBase {
+
+private:
+  /// \brief Analysis context
+  Context& _ctx;
+
+  /// \brief Analyzed function
+  ar::Function* _function;
+
+  /// \brief Current call context
+  CallContext* _call_context;
+
+  /// \brief Fixpoint parameters
+  const CodeFixpointParameters& _fixpoint_parameters;
+
+public:
+  /// \brief Constructor for an entry point
+  ///
+  /// \param ctx Analysis context
+  /// \param checkers List of checkers to run
+  /// \param entry_point Function to analyze
+  FunctionFixpointBase(Context& ctx,
+                   ar::Function* entry_point) noexcept
+      : _ctx(ctx), _function(entry_point),
+        _call_context(ctx.call_context_factory->get_empty()),
+        _fixpoint_parameters(ctx.fixpoint_parameters->get(entry_point))
+  {
+
+  }
+
+};
+
+} // end namespace analyzer
+} // end namespace ikos
+
+#endif // IKOS_FUNCTION_FIXPOINT_BASE_HPP
---
+++ analyzer/src/analysis/function_fixpoint_base.cpp
@@ -0,0 +1,3 @@
+//
+// Created by fordrl on 11/23/21.
+//
